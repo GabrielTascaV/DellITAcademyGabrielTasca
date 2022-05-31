f827b8f67f1dccb926cd1db8432c4f7862f7edc3
GabrielTascaV <gabriel.villa@edu.pucrs.br>
2022-05-30 23:57:19 -0300

    Todos funcionando e tratamento de compos null

--- src/App.py
+++ src/App.py
@@ -6,9 +6,12 @@ def consulta_nome(data_set,nome):
     dados = []
     #Adiciona na variavel dados se o campo COMERCIALIZAÇÃO 2020 for sim e o nome for igual a coluna PRODUTO
     for i in data_set:
-        if(i[7] == 'Sim' and i[2] == nome):
+        if(i[7] == 'Sim' and i[2] == nome.upper()):
             dados.append(i)
     #Retorna a variável dados com os medicamento que foram encontrados
+    print("Resultado: ")
+    for i in range(len(dados)):
+        print(dados[i])
     return dados
 
 
@@ -23,7 +26,10 @@ def consulta_codigo_barras(data_set,codigo_barras):
     for i in data_set:
         if(i[1] == codigo_barras):
             obj = i
-            pmc0 = float(i[5].replace(',','.'))
+            if(i[5] == ''):
+                print("Valor nulo na tabela. Substituído por 0.")
+                obj[5] = '0'
+            pmc0 = float(obj[5].replace(',','.'))
             if(pmc0 > cont_max):
                 max = pmc0
             if(pmc0 < cont_min):
@@ -31,12 +37,16 @@ def consulta_codigo_barras(data_set,codigo_barras):
             break
     for k in data_set:
         if(k[2] == obj[2]):
+            if(k[5] == ''):
+                print("Valor nulo na tabela. Substituído por 0.")
+                k[5] = '0'
             pmc0 = float(k[5].replace(',','.'))
             if(pmc0 > max):
                 max = pmc0
             if(pmc0 < min):
                 min = pmc0
     dif = max - min
+    print("Resultado: ")
     print('Valor Máximo: %.2f' %max)
     print('Valor Mínimo: %.2f' %min)
     print('Diferença: %.2f' %dif)
@@ -61,6 +71,7 @@ def consulta_lista_concessao(data_set):
     s_cont_negativa = '%.2f' %cont_negativa
     s_cont_neutra = '%.2f' %cont_neutra
     s_cont_positiva = '%.2f' %cont_positiva
+    print("Resultado: ")
     print('CLASSIFICAÇÃO    PERCENTUAL       GRÁFICO')
     print('Negativa         '+ s_cont_negativa + '%           ' + '*'*int(cont_negativa))
     print('Neutra           '+ s_cont_neutra   +'%            ' +  '*'*int(cont_neutra))
@@ -101,27 +112,43 @@ def ler_arquivo(dados,name):
         return dados
     except FileNotFoundError:
         print("Arquivo não encontrado")
+        return 0
 
 data_set = []
-
 data_set = ler_arquivo(data_set,'DellITAcademyGabrielTasca/TA_PRECO_MEDICAMENTO.csv')
-print("1 - Consulte medicamento por nome")
-print("2 - Consultar o preço máximo e mínimo por código de barras")
-print("3 - Consultar a Lista de Concessão de Crédito Tributário (PIS/COFINS)")
-print("DIGITE UM NÚMERO: ")
-option = input()
-
-if(option == '1'):
-    print("Digite o nome do medicamento: ")
-    nome = input()
-    dados = consulta_nome(data_set,nome)
-    for i in range(len(dados)):
-        print(dados[i])
-elif(option == '2'):
-    print("Digite o código de barras: ")
-    codigo_barras = input()
-    consulta_codigo_barras(data_set,codigo_barras)
-elif(option == '3'):
-    consulta_lista_concessao(data_set)
+out = False
+while(not(out)):
+    print("1 - Consulte medicamento por nome")
+    print("2 - Consultar o preço máximo e mínimo por código de barras")
+    print("3 - Consultar a Lista de Concessão de Crédito Tributário (PIS/COFINS)")
+    print("0 - Sair do programa")
+    print("DIGITE UM NÚMERO: ")
+    option = input()
+    if(option == '1'):
+        print("Digite o nome do medicamento: ")
+        nome = input()
+        consulta_nome(data_set,nome)
+        print("")
+        print("")
+        print("")
+    elif(option == '2'):
+        print("Digite o código de barras: ")
+        codigo_barras = input()
+        consulta_codigo_barras(data_set,codigo_barras)
+        print("")
+        print("")
+        print("")
+    elif(option == '3'):
+        consulta_lista_concessao(data_set)
+        print("")
+        print("")
+        print("")
+    elif(option == '0'):
+        out = True
+    else:
+        print("Erro, nenhum valor digitado. Digite um valor válido")
+        print("")
+        print("")
+        print("")
 
 
